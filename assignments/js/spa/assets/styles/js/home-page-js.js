9f9e53f47c237f55b3cd346de6c3f0818e60e330
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-09-21 17:46:52 +0530

    added main sections(customer-page)

--- assignments/js/spa/assets/styles/js/home-page-js.js
+++ assignments/js/spa/assets/styles/js/home-page-js.js
@@ -1,8 +1,17 @@
 $('#a-customers').click(function () {
     $('#customer-page-section').css('visibility','visible');
+    $('#item-page-section').css('visibility','hidden');
+    $('#home-page-section').css('visibility','hidden');
 });
 
 $('#a-home').click(function () {
     $('#home-page-section').css('visibility','visible');
     $('#customer-page-section').css('visibility','hidden');
+    $('#item-page-section').css('visibility','hidden');
+});
+
+$('#a-items').click(function () {
+    $('#item-page-section').css('visibility','visible');
+    $('#customer-page-section').css('visibility','hidden');
+    $('#home-page-section').css('visibility','hidden');
 });
