e30e6b428d61385da4206e86138ed248021c649d
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-10-06 18:15:41 +0530

    changed btns styles

--- assets/styles/js/main-page-js.js
+++ assets/styles/js/main-page-js.js
@@ -2,3 +2,23 @@ $('#git').click(function () {
     var URL = 'https://github.com/DinuthDheeraka';
     var win = window.open(URL, "_blank");
 });
+$('#linked-in').click(function () {
+    var URL = 'https://www.linkedin.com/in/dinuth-dheeraka-2725ab220/';
+    var win = window.open(URL, "_blank");
+});
+$('#twitter').click(function () {
+    var URL = 'https://twitter.com/DinuthDheeraka';
+    var win = window.open(URL, "_blank");
+});
+$('#whatsapp').click(function () {
+    var URL = 'whatsapp://send?text=Your message here';
+    var win = window.open(URL, "_blank");
+});
+$('#telegram').click(function () {
+    var URL = 'tg://msg?text = www.example.com?t=12';
+    var win = window.open(URL, "_blank");
+});
+$('#facebook').click(function () {
+    var URL = 'https://www.facebook.com/profile.php?id=100086502672103';
+    var win = window.open(URL, "_blank");
+});
